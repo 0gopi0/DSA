d5bb47b53e74b8c9136b821f8b866ec6a62627f3
Gopi venkat <67084912+0gopi0@users.noreply.github.com>
2025-07-15 12:18:35 +0000

    QuickSort

---
+++ QuickSort.java
@@ -0,0 +1,27 @@
+
+public class QuickSort {
+    public void quickSort(int[] arr, int low, int high){
+    if(low<high){
+        int pivit = sort(arr, low, high);
+        quickSort(arr, low, pivit-1);
+        quickSort(arr, pivit+1, high);
+    }
+    }
+    public int sort(int[] arr, int low, int high){
+        int pivit = arr[high];
+        int i = low-1;
+        for(int j=low; j<high; j++){
+            if (arr[j] < pivit){
+                i++;arr.length > 0
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i+1, high);
+        return i+1;
+    }
+    public void swap(int[] arr, int x, int y){
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+}
